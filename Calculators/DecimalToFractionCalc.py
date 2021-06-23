1948b37527fd04f036a390d1332ac6865421e70c
Krushay Bhavsar <krushaybhavsar@gmail.com>
2021-06-22 20:20:12 -0400

    Updated Documentation

--- Calculators/DecimalToFractionCalc.py
+++ Calculators/DecimalToFractionCalc.py
@@ -4,3 +4,6 @@ for numer in range(1, 100):
     for denom in range(1, 100):
         if (numer/denom) == decimal:
             print(f"The decimal is equivalent to {numer}/{denom}")
+            
+# This is an inefficient approach since you can just use float.as_integer_ratio()
+# The purpose of the excerise, however, is to get students thinking about nested for loops and code logic
